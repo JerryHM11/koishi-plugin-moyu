ea3ac6a47331a3f602f4fd76e622de433e549748
JerryHM11 <1299640312@qq.com>
2022-11-01 22:54:38 +0800

    修改部分代码

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.apply = exports.Config = exports.name = void 0;
 const koishi_1 = require("koishi");
-exports.name = 'weather';
+exports.name = 'moyu';
 exports.Config = koishi_1.Schema.object({});
 
 function apply(ctx) {
@@ -11,7 +11,6 @@ function apply(ctx) {
     .action(async ({ options }) => {
         try {
             const data = await ctx.http.get('https://api.vvhan.com/api/moyu', {responseType: 'arraybuffer'})
-            console.log(koishi_1.segment.image(data));
             return koishi_1.segment.image(data);
         } catch (error) {
             ctx.logger('tools').warn(error);
